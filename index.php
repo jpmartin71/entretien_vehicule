b126cc62c150382c8ee04bb5897132011322f0ea
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-02 13:32:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 # test.php
 ini_set("display_errors", 1);
-//ini_set("track_errors", 1);
-//ini_set("html_errors", 1);
-ini_set('display_startup_errors',1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+//ini_set('display_startup_errors',1);
 error_reporting(E_ALL)
 
 foreach ($_GET as $key => $value) $_GET[$key]=htmlspecialchars($value);
