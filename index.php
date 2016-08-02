0adcbf0228e1714b8edff28eeff937e61f710995
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-02 13:31:54 +0200

    Update index.php
    display-errors

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 # test.php
 ini_set("display_errors", 1);
-ini_set("track_errors", 1);
-ini_set("html_errors", 1);
+//ini_set("track_errors", 1);
+//ini_set("html_errors", 1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL)
 
