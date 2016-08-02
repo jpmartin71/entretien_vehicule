a006b3f38ea98e6b8dcbf3e5be2697c890b31fb2
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-02 14:41:59 +0200

    Update formulaire_operation.php

--- view/formulaire_operation.php
+++ view/formulaire_operation.php
@@ -26,29 +26,29 @@
 						<input type="text" name="denomination" id="denomination" value="<?php echo $operation['denomination'];?>">
 
 						<legend for="periodicite_km">Périodicité (en km)</legend>
-						<input type="number" name="periodicite_km" id="periodicite_km">
+						<input type="number" name="periodicite_km" id="periodicite_km" value="<?php echo $operation['periodicite_km'];?>">
 
 						<legend for="periodicite_tps">Périodicité (en mois)</legend>
-						<input type="number" name="periodicite_tps" id="periodicite_tps">
+						<input type="number" name="periodicite_tps" id="periodicite_tps" value="<?php echo $operation['periodicite_tps'];?>">
 					</div>
 					<div class="form_operation">
 						<legend for="effectuee_km">Effectué à</legend>
-						<input type="number" name="effectuee_km" id="effectuee_km">
+						<input type="number" name="effectuee_km" id="effectuee_km" value="<?php echo $operation['effectuee_km'];?>">
 
 						<legend for="effectuee_tps">Effectué le</legend>
-						<input type="date" name="effectuee_tps" id="effectuee_tps">
+						<input type="date" name="effectuee_tps" id="effectuee_tps" value="<?php echo $operation['effectuee_tps'];?>">
 
 						<legend for="echeance_km">Prochaine échéance (km)</legend>
-						<input type="number" name="echeance_km" id="echeance_km">
+						<input type="number" name="echeance_km" id="echeance_km" value="<?php echo $operation['echeance_km'];?>">
 
 						<legend for="echeance_date">Prochaine échéance (date)</legend>
-						<input type="date" name="echeance_date" id="echeance_date">
+						<input type="date" name="echeance_date" id="echeance_date" value="<?php echo $operation['echeance_tps'];?>">
 
 						
 					</div>
 
 						<legend for="obs">Observations:</legend>
-						<textarea></textarea>
+						<textarea name="obs" id="obs" ><?php echo $operation['obs'];?></textarea>
 					
 						<input type="hidden" name="" id="" value="">
 						<input type="submit" valuer="Créer">
