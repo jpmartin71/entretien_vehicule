f0b25c5cddeae75bb74a7053ed1393b9d9b5391f
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-02 10:54:18 +0200

    Update formulaire_operation.php

--- controler/formulaire_operation.php
+++ controler/formulaire_operation.php
@@ -4,57 +4,57 @@ echo "controler formulaire opération";
 include_once './model/formulaire_operation.php';
 
 //$operation pour le remplissage des champs
-$operation=array('id_vehicule'=>, 'denomination'=>, 'periodicite_km'=>, 'periodicite_tps'=>, 'effectuee_km'=>, 'effectuee_tps'=>, 'echeance_km'=>, 'echeance_date'=>, 'obs'=>);
-echo '$operation:';print_r($operation);echo '</br>';
+//$operation=array('id_vehicule'=>, 'denomination'=>, 'periodicite_km'=>, 'periodicite_tps'=>, 'effectuee_km'=>, 'effectuee_tps'=>, 'echeance_km'=>, 'echeance_date'=>, 'obs'=>);
+//echo '$operation:';print_r($operation);echo '</br>';
 
 //verification des information du post avant de creer la ligne sql ou renvoie au formulaire pour correction
 if(isset($_GET['action']) and $_GET['action']=='create_operation')
-	{
-		$vehicule=get_infos_vehicule($_POST['id_vehicule']);
-		echo '$vehicule:';print_r($vehicule);echo '</br>';
-		
-		if(isset($_POST['id_vehicule']) and !empty($vehicule))
-		{//si id_vehicule est present et que le vehicule existe
-			$operation['id_vehicule']=$vehicule['id'];
-		}
-		else 
-		{
-			$operation['id_vehicule']=0;
-			$erreur+=1;
-		}
-		
-		if(isset($_POST['denomination']) and !empty($_POST['denomination']))
-		{//si denomination est present et que denomination n'est pas vide
-			$operation['denomination']=$_POST['denomination'];
-		}
-		else
-		{
-			$operation['denomination']="";
-			$erreur+=2;
-		}
-		
-		if(isset($_POST['periodicite_km']) and !empty($_POST['periodicite_km']) and is_numeric($_POST['periodicite_km']))
-		{//si periodicite_km est present et que periodicite_km n'est pas vide et est un nombre
-			$operation['periodicite_km']=$_POST['periodicite_km'];
-		}
-		elseif(isset($_POST['inib_periodicite_km']))
-		{//si l'operation n'est pas soumise à une périodicité en km
-			$operation['periodicite_km']=0;
-		}
-		else
-		{
-			$operation['periodicite_km']=0;
-			$erreur+=4;
-		}
-		
-		
-		
-		
-		echo '$operation:';print_r($operation);echo '</br>';
-		echo '$erreur:';print_r($erreur);echo '</br>';
-		
-		
+{
+	$vehicule=get_infos_vehicule($_POST['id_vehicule']);
+	echo '$vehicule:';print_r($vehicule);echo '</br>';
+	
+	if(isset($_POST['id_vehicule']) and !empty($vehicule))
+	{//si id_vehicule est present et que le vehicule existe
+		$operation['id_vehicule']=$vehicule['id'];
 	}
+	else 
+	{
+		$operation['id_vehicule']=0;
+		$erreur+=1;
+	}
+	
+	if(isset($_POST['denomination']) and !empty($_POST['denomination']))
+	{//si denomination est present et que denomination n'est pas vide
+		$operation['denomination']=$_POST['denomination'];
+	}
+	else
+	{
+		$operation['denomination']="";
+		$erreur+=2;
+	}
+	
+	if(isset($_POST['periodicite_km']) and !empty($_POST['periodicite_km']) and is_numeric($_POST['periodicite_km']))
+	{//si periodicite_km est present et que periodicite_km n'est pas vide et est un nombre
+		$operation['periodicite_km']=$_POST['periodicite_km'];
+	}
+	elseif(isset($_POST['inib_periodicite_km']))
+	{//si l'operation n'est pas soumise à une périodicité en km
+		$operation['periodicite_km']=0;
+	}
+	else
+	{
+		$operation['periodicite_km']=0;
+		$erreur+=4;
+	}
+	
+	
+	
+	
+	echo '$operation:';print_r($operation);echo '</br>';
+	echo '$erreur:';print_r($erreur);echo '</br>';
+	
+	
+}
 	
 $liste_vehicules=get_liste_vehicule();
 if(!empty($liste_vehicules))
