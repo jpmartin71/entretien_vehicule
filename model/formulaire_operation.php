71a1036690361b251aa1a29e675000b41c339ef0
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-02 13:20:03 +0200

    Update formulaire_operation.php

--- model/formulaire_operation.php
+++ model/formulaire_operation.php
@@ -11,3 +11,14 @@
 
 		return $liste_vehicule;
 	}
+	
+//recuper lis information d'un vehicule	
+function get_infos_vehicule($id_vehicule)
+{
+	global $bdd;
+	$req=$bdd->prepare('SELECT * FROM vehicules WHERE id=:id');
+	$req->execute(array('id' => $id_vehicule));
+	$infos=$req->fetch();
+	$req->closecursor();
+return $infos;
+}
