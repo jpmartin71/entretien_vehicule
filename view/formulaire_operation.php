d02c219e0b692f80d06a42f922b65baf8404b58b
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-04 09:31:50 +0200

    Update formulaire_operation.php

--- view/formulaire_operation.php
+++ view/formulaire_operation.php
@@ -34,14 +34,14 @@
 							<label for="periodicite_km">Périodicité (en km)</label>
 							<input type="number" name="periodicite_km" id="periodicite_km" value="<?php echo $operation['periodicite_km'];?>" <?php if(isset($_POST['inib_periodicite_km']))echo 'disabled';?>>
 							</br>
-							<input type="checkbox" name="inib_periodicite_km" id="inib_periodicite_km" <?php if(isset($_POST['inib_periodicite_km']))echo 'checked';?> />
+							<input type="checkbox" onchange="checkbox_inib_periodicite_km()" name="inib_periodicite_km" id="inib_periodicite_km" <?php if(isset($_POST['inib_periodicite_km']))echo 'checked';?> />
 							<label for="inib_periodicite_km" class="label_checkbox">Pas de périodicité kilométrique</label>
 							</br>
 							
 							<label for="periodicite_tps">Périodicité (en mois)</label>
 							<input type="number" name="periodicite_tps" id="periodicite_tps" value="<?php echo $operation['periodicite_tps'];?>" <?php if(isset($_POST['inib_periodicite_tps']))echo 'disabled';?>>
 							</br>
-							<input type="checkbox" name="inib_periodicite_tps" id="inib_periodicite_tps" <?php if(isset($_POST['inib_periodicite_tps']))echo 'checked';?>/>
+							<input type="checkbox" onchange="checkbox_inib_periodicite_tps()" name="inib_periodicite_tps" id="inib_periodicite_tps" <?php if(isset($_POST['inib_periodicite_tps']))echo 'checked';?>/>
 							<label for="inib_periodicite_tps" class="label_checkbox">Pas de périodicité temporelle</label>
 						</fieldset>
 						
@@ -50,7 +50,7 @@
 						<fieldset>
 							<legend>Opération effectuée:</legend>
 							
-							<input type="checkbox" name="inib_effectuee" id="inib_effectuee" <?php if(isset($_POST['inib_effectuee']))echo 'checked';?>/>
+							<input type="checkbox" onchange="checkbox_inib_operation_effectuee()" name="inib_effectuee" id="inib_effectuee" <?php if(isset($_POST['inib_effectuee']))echo 'checked';?>/>
 							<label for="inib_effectuee" class="label_checkbox">Opération jamais éffectuée.</label>
 							</br>
 							
@@ -94,6 +94,32 @@
 		</section>
 		
 		
+        <script type="text/javascript">
+		function checkbox_inib_periodicite_km()
+		{ 
+			if(document.getElementById("inib_periodicite_km").checked)document.getElementById("periodicite_km").disabled = true;
+			else document.getElementById("periodicite_km").disabled = false;
+		}
+        	function checkbox_inib_periodicite_tps()
+		{ 
+			if(document.getElementById("inib_periodicite_tps").checked)document.getElementById("periodicite_tps").disabled = true;
+			else document.getElementById("periodicite_tps").disabled = false;
+		}
+		function checkbox_inib_operation_effectuee()
+		{ 
+			if(document.getElementById("inib_effectuee").checked)
+			{
+				document.getElementById("effectuee_km").disabled = true;
+				document.getElementById("effectuee_date").disabled = true;
+			}
+			else 
+			{
+				document.getElementById("effectuee_km").disabled = false;
+				document.getElementById("effectuee_date").disabled = false;
+			}
+		}
         
+
+    </script>
 	</body>
 </html>
