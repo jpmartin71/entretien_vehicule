22acd459fc9173aa9340bde2fe55740aba8422b7
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-04 08:31:44 +0200

    Update formulaire_operation.php

--- view/formulaire_operation.php
+++ view/formulaire_operation.php
@@ -21,39 +21,58 @@
 							<?php
 								foreach($liste_vehicules as $vehicule)?> <option value='<?php echo $vehicule['id'];?>'><?php echo $vehicule['libelle'];?></option>
 						</select>
+						</br>
 						
 						<label for="denomination">Dénomination:</label>
 						<input type="text" name="denomination" id="denomination" value="<?php echo $operation['denomination'];?>">
 						
 						<fieldset>
 							<legend>Périodicité</legend>
+							
 							<label for="periodicite_km">Périodicité (en km)</label>
 							<input type="number" name="periodicite_km" id="periodicite_km" value="<?php echo $operation['periodicite_km'];?>">
 							</br>
+							
 							<input type="checkbox" name="inib_periodicite_km" id="inib_periodicite_km" />
 							<label for="inib_periodicite_km">Pas de périodicité kilométrique</label>
+							</br>
 							
 							<label for="periodicite_tps">Périodicité (en mois)</label>
 							<input type="number" name="periodicite_tps" id="periodicite_tps" value="<?php echo $operation['periodicite_tps'];?>">
 							</br>
+							
 							<input type="checkbox" name="inib_periodicite_tps" id="inib_periodicite_tps" />
 							<label for="inib_periodicite_tps">Pas de périodicité temporelle</label>
 						</fieldset>
 						
 					</div>
 					<div class="form_operation">
-						<label for="effectuee_km">Effectué à</label>
-						<input type="number" name="effectuee_km" id="effectuee_km" value="<?php echo $operation['effectuee_km'];?>">
-
-						<label for="effectuee_date">Effectué le</label>
-						<input type="date" name="effectuee_date" id="effectuee_date" value="<?php echo date_create($operation['effectuee_date'])->format('d/m/Y');?>">
-
-						<label for="echeance_km">Prochaine échéance (km)</label>
-						<input type="number" name="echeance_km" id="echeance_km" value="<?php echo $operation['echeance_km'];?>">
-
-						<label for="echeance_date">Prochaine échéance (date)</label>
-						<input type="date" name="echeance_date" id="echeance_date" value="<?php echo $operation['echeance_date'];?>" enable=false>
-
+						<fieldset>
+							<legend>Opération effectuée:</legend>
+							
+							<label for="effectuee_km">Effectué à (en km):</label>
+							<input type="number" name="effectuee_km" id="effectuee_km" value="<?php echo $operation['effectuee_km'];?>">
+							</br>
+							
+							<label for="effectuee_date">Effectué le (jj/mm/aaaa):</label>
+							<input type="date" name="effectuee_date" id="effectuee_date" value="<?php echo date_create($operation['effectuee_date'])->format('d/m/Y');?>">
+							</br>
+							
+							
+						</fieldset>
+						
+						<fieldset>
+							<legend>Prochaine écheance:</legend>
+							
+							<label for="echeance_km">Prochaine échéance (km)</label>
+							<input type="number" name="echeance_km" id="echeance_km" value="<?php echo $operation['echeance_km'];?>"  disabled>
+							</br>
+							
+							<label for="echeance_date">Prochaine échéance (date)</label>
+							<input type="date" name="echeance_date" id="echeance_date" value="<?php echo $operation['echeance_date'];?>"  disabled>
+						</fieldset>
+							
+							
 						
 					</div>
 
