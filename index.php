d6c5622384f0b765f8a4c95602b807dc96e3f1a7
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-04 19:12:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set("display_errors", 1);
 ini_set("track_errors", 1);
 ini_set("html_errors", 1);
 ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 
 foreach ($_GET as $key => $value) $_GET[$key]=htmlspecialchars($value);
 foreach ($_POST as $key => $value) $_POST[$key]=htmlspecialchars($value);
