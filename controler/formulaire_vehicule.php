099f21b3244c5f74fe44d95ec0a1056e35149f53
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-05 11:12:43 +0200

    Update formulaire_vehicule.php

--- controler/formulaire_vehicule.php
+++ controler/formulaire_vehicule.php
@@ -2,6 +2,7 @@
 
 echo '</br><strong>Controler formulaire vehicule</strong>';
 
+include_once './model/fonctions_vehicule.php';
 $vehicule=array('id'=>null,'marque'=>null,'modele'=>null,'date_1_immat'=>null,'date_achat'=>null,'km_achat'=>null,'vin'=>null,'immatriculation'=>null);
 	
 if(isset($_GET['action']) and $_GET['action']=='create_vehicule')
