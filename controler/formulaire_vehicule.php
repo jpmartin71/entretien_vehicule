aec7ee1ee63fee4fc9248d670f992032187016b2
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-05 11:02:57 +0200

    commit general

--- controler/formulaire_vehicule.php
+++ controler/formulaire_vehicule.php
@@ -6,10 +6,6 @@ $vehicule=array('id'=>null,'marque'=>null,'modele'=>null,'date_1_immat'=>null,'d
 	
 if(isset($_GET['action']) and $_GET['action']=='create_vehicule')
 {
-<<<<<<< HEAD
-		  include_once './controler/create_vehicule.php'
-}
-=======
 		  include_once './controler/create_vehicule.php';
 }
 
@@ -19,4 +15,4 @@ elseif(isset($_GET['action']) and $_GET['action']=='udate_vehicule')
 }
 
 include_once './view/formulaire_vehicule.php';
->>>>>>> 14716d079b3738ea35c4bd36d072fe55ace63790
+
