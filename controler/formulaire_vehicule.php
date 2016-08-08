29f1bacf11ef221966e2f57c9d079978ea6120f7
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-08 11:56:19 +0200

    Update formulaire_vehicule.php

--- controler/formulaire_vehicule.php
+++ controler/formulaire_vehicule.php
@@ -13,6 +13,10 @@ if (isset($_GET['form']) and $_GET['form']=='create_vehicule')
 }
 elseif(isset($_GET['form']) and $_GET['form']=='update_vehicule') 
 {
+	if(isset($_GET['vehicule']) and !empty($_GET['vehicule'])  and !empty($vehicule=get_infos_vehicule($_GET['vehicule'])) )
+	{
+echo '</br>$vehicule:';print_r($vehicule);
+	}
 	$action_form='?action=update_vehicule';
 	$text_submit='Mettre à jour le vehicule';
 }
