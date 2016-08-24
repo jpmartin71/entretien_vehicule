03c7f35174e5e41f18c165095d1841fce573aa61
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-24 08:59:27 +0200

    Update formulaire_operation.php

--- view/formulaire_operation.php
+++ view/formulaire_operation.php
@@ -13,15 +13,16 @@
 		<section>
 			<article >
 				<form class="form_operation" method="post" action='<?php echo $action; ?>'>
+					<h2><?php echo $titre_formulaire; ?></h2>
 					<?php if((isset($_GET['form']) and $_GET['form']=='update_operation') or (isset($_GET['action']) and $_GET['action']=='update_operation'))
 					{?>
-						<h2>Modification de l'opération</h2>
+						
 						<div class="form_operation retour_operation">
 							<a href="#">lien à definir</a>
 						</div> 
 					<?php
 					}
-					else{?> <h2>Création d'une opération</h2> <?php } ?>
+					
 					<fieldset>
 						<div class="form_operation <?php if(isset($erreur_view['vehicule']))echo 'erreur';?>">
 							<?php if(isset($erreur_view['vehicule']))echo $erreur_view['vehicule'];?>
@@ -73,10 +74,10 @@
 							<div class="form_operation">
 								<?php if(isset($erreur_view['periodicite_tps']))echo $erreur_view['periodicite_tps'];?>		
 								<label for="periodicite_tps">Périodicité (en mois)</label>
-								<input type="number" name="periodicite_tps" id="periodicite_tps" value="<?php echo $operation['periodicite_tps'];?>" <?php if(isset($_POST['inib_periodicite_tps']))echo 'disabled';?>>
+								<input type="number" name="periodicite_tps" id="periodicite_tps" value="<?php echo $operation['periodicite_tps'];?>" <?php echo $enable_input['periodicite_tps']; ?>>
 							</div>
 							<div class="form_operation">
-								<input type="checkbox" onchange="checkbox_inib_periodicite_tps()" name="inib_periodicite_tps" id="inib_periodicite_tps" <?php if(isset($_POST['inib_periodicite_tps']))echo 'checked';?>/>
+								<input type="checkbox" onchange="checkbox_inib_periodicite_tps()" name="inib_periodicite_tps" id="inib_periodicite_tps" <?php echo $enable_input['inib_periodicite_tps']; ?> <?php if(isset($_POST['inib_periodicite_tps']))echo 'checked';?>/>
 								<label for="inib_periodicite_tps" class="label_checkbox">Pas de périodicité temporelle</label>
 							</div>
 						</div>
@@ -85,19 +86,19 @@
 					<fieldset>
 						<legend>Opération effectuée:</legend>
 						
-							<input type="checkbox" onchange="checkbox_inib_operation_effectuee()" name="inib_effectuee" id="inib_effectuee" <?php if(isset($_POST['inib_effectuee']))echo 'checked';?>/>
+							<input type="checkbox" onchange="checkbox_inib_operation_effectuee()" name="inib_effectuee" id="inib_effectuee" <?php echo $enable_input['inib_effectuee']; ?> <?php if(isset($_POST['inib_effectuee']))echo 'checked';?>/>
 							<label for="inib_effectuee" class="label_checkbox">Opération jamais éffectuée.</label>
 						
 						<div class="form_operation <?php if(isset($erreur_view['effectuee_km']))echo 'erreur';?>">
 							<?php if(isset($erreur_view['effectuee_km']))echo $erreur_view['effectuee_km'];?>
 							<label for="effectuee_km">Effectué à (en km):</label>
-							<input type="number" name="effectuee_km" id="effectuee_km" value="<?php echo $operation['effectuee_km'];?>" <?php if(isset($_POST['inib_effectuee']))echo 'disabled';?>>
+							<input type="number" name="effectuee_km" id="effectuee_km" value="<?php echo $operation['effectuee_km'];?>" <?php echo $enable_input['effectuee_km']; ?>>
 						</div>
 						
 						<div class="form_operation <?php if(isset($erreur_view['effectuee_date']))echo 'erreur';?>">
 							<?php if(isset($erreur_view['effectuee_date']))echo $erreur_view['effectuee_date'];?>
 							<label for="effectuee_date">Effectué le (jj/mm/aaaa):</label>
-							<input type="date" name="effectuee_date" id="effectuee_date" value="<?php if($operation['effectuee_date']!=null)echo date_create($operation['effectuee_date'])->format('Y-m-d');?>" <?php if(isset($_POST['inib_effectuee']))echo 'disabled';?>>
+							<input type="date" name="effectuee_date" id="effectuee_date" value="<?php if($operation['effectuee_date']!=null)echo date_create($operation['effectuee_date'])->format('Y-m-d');?>" <?php echo $enable_input['effectuee_date']; ?>>
 							</br>
 						</div>
 							
@@ -107,11 +108,11 @@
 						<legend>Prochaine écheance:</legend>
 						<div class="form_operation">
 							<label for="echeance_km">Prochaine échéance (km)</label>
-							<input type="number" name="echeance_km" id="echeance_km" value="<?php echo $operation['echeance_km'];?>"  disabled>
+							<input type="number" name="echeance_km" id="echeance_km" value="<?php echo $operation['echeance_km'];?>"  <?php echo $enable_input['echeance_km']; ?>>
 						</div>
 						<div class="form_operation">
 							<label for="echeance_date">Prochaine échéance (date)</label>
-							<input type="date" name="echeance_date" id="echeance_date" value="<?php if($operation['echeance_date']!=null)echo date_create($operation['echeance_date'])->format('Y-m-d');?>"  disabled>
+							<input type="date" name="echeance_date" id="echeance_date" value="<?php if($operation['echeance_date']!=null)echo date_create($operation['echeance_date'])->format('Y-m-d');?>"  <?php echo $enable_input['echeance_date']; ?>>
 						</div>
 					</fieldset>
 					<fieldset>
