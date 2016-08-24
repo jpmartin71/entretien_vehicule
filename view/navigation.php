45641e4fcc85b4a195e0f720c257517d7de92894
jpmartin71 <jpmartin.7183@gmail.com>
2016-08-24 11:15:50 +0200

    Update navigation.php

--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,6 @@
 <nav>
 	<ul>
-		<li><a href="index.php?">Acceuil</a></li>
+		<li><a href="index.php?">Accueil</a></li>
 		<li><a href="?form=new_operation">Créer une opération</a></li>
 		<li><a href="?form=create_vehicule">Créer un véhicule</a></li>
 	</ul>
